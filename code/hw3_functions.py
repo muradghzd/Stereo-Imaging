f2d6917f863678bc49cb909b9bb90638e57a5aa2
Murad Aghazada <murad.aghazada02@gmail.com>
2022-10-27 04:11:22 +0900

    Fix errors and clean code

--- code/hw3_functions.py
+++ code/hw3_functions.py
@@ -55,14 +55,9 @@ def calculate_fundamental_matrix(pts1, pts2):
     assert pts1.shape[0]==pts2.shape[0]
     # Your code here
     ################################################
-    x1 = np.array([*pts1[2], 1])
-    x2 = np.array([*pts2[2], 1])
-    x1 = np.reshape(x1, (1,3))
-    x2 = np.reshape(x2, (3,1))
-    
     pts1, T1 = normalize_points(pts1.T, 2)
     pts2, T2 = normalize_points(pts2.T, 2)
-    print(f"Shape of T1: {T1.shape}")
+
     pts1_x = pts1[0] 
     pts1_y = pts1[1]
     pts2_x = pts2[0]
@@ -71,27 +66,16 @@ def calculate_fundamental_matrix(pts1, pts2):
     A = np.concatenate((pts2_x*pts1_x, pts2_x*pts1_y, pts2_x,
                         pts2_y*pts1_x, pts2_y*pts1_y, pts2_y,
                         pts1_x, pts1_y, np.ones(8)))
-    print(f"Shape of A: {A.shape}")
-    A = np.reshape(A, (8,9))
-    AT_A = A.T @ A
+    A = np.reshape(A, (9,8))
+    AT_A = A @ A.T
     eig_val, eig_vec = np.linalg.eig(AT_A)
     min_idx = np.argsort(eig_val)[0]
     f = eig_vec[:, min_idx]
     F = np.reshape(f, (3,3))   
     S, V, D = np.linalg.svd(F)
-    print(f"V vales: {V}")
     V[2] = 0
-    # fundamental_matrix = np.matmul(np.matmul(S, np.diag([*V[:2], 0])), D)
     fundamental_matrix = S @ np.diag(V) @ D
     fundamental_matrix = T2.T @ fundamental_matrix @ T1
-    
-    print(f"shape of x1: {x1.shape}") 
-    print(f"shape of x2: {x2.shape}")
-    print(f"sihape of fundamental matrix: {fundamental_matrix.shape}")
-    res = x1 @ fundamental_matrix @ x2
-    # res = x1@fundamental_matrix@x2
-    # res = np.matmul(x1, fundamental_matrix, x2)
-    print(f"Result: {res}")
     ################################################################
     return fundamental_matrix
 
