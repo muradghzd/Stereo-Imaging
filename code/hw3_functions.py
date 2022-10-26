36dbe2c09c9cbfc600d2dc7623c539ebc0783cf1
Murad Aghazada <murad.aghazada02@gmail.com>
2022-10-27 00:25:21 +0900

    Complete calculate_fundamental_matrix

--- code/hw3_functions.py
+++ code/hw3_functions.py
@@ -10,6 +10,7 @@
 # set "VISUALIZE = True" in "hw3_main.py" to check your results.
 ################################################################
 from locale import normalize
+from turtle import shape
 from utils import normalize_points
 import numpy as np
 import cv2
@@ -27,8 +28,8 @@ AGG_FILTER_SIZE = 5
 def bayer_to_rgb_bilinear(bayer_img):
     ################################################################
     rgb_img = None
-
-
+    # print(f"Matrix: {bayer_img}")
+    # print(f"Shape of bayer img: {bayer_img.shape}")
     ################################################################
     return rgb_img
 
@@ -54,9 +55,42 @@ def calculate_fundamental_matrix(pts1, pts2):
     assert pts1.shape[0]==pts2.shape[0]
     # Your code here
     ################################################
+    pts1 = normalize_points(pts1.T, 2)[0].T
+    pts2 = normalize_points(pts2.T, 2)[0].T
+
+    pts1_x = pts1[:,0]
+    pts1_y = pts1[:,1]
+    pts2_x = pts2[:,0]
+    pts2_y = pts2[:,1]
     
+    A = np.concatenate((pts2_x*pts1_x, pts2_x*pts1_y, pts2_x,
+                        pts2_y*pts1_x, pts2_y*pts1_y, pts2_y,
+                        pts1_x, pts1_y, np.ones(8)))
+    print(f"Shape of A: {A.shape}")
+    A = np.reshape(A, (8,9))
+    AT_A = A.T @ A
+    eig_val, eig_vec = np.linalg.eig(AT_A)
+    min_idx = np.argsort(eig_val)[0]
+    f = eig_vec[:, min_idx]
+    F = np.reshape(f, (3,3))   
+    S, V, D = np.linalg.svd(F)
+    print(f"V vales: {V}")
+    V[2] = 0
+    # fundamental_matrix = np.matmul(np.matmul(S, np.diag([*V[:2], 0])), D)
+    fundamental_matrix = S @ np.diag(V) @ D
+    x1 = np.array([*pts1[0], 1])
+    x2 = np.array([*pts2[0], 1])
+    x1 = np.reshape(x1, (1,3))
+    x2 = np.reshape(x2, (3,1))
+    print(f"shape of x1: {x1.shape}") 
+    print(f"shape of x2: {x2.shape}")
+    print(f"sihape of fundamental matrix: {fundamental_matrix.shape}")
+    res = np.matmul(np.matmul(x1, fundamental_matrix), x2)
+    # res = x1@fundamental_matrix@x2
+    # res = np.matmul(x1, fundamental_matrix, x2)
+    print(f"Result: {res}")
     ################################################################
-    return None
+    return fundamental_matrix
 
 
 
